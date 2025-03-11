f14a6e4a3746129a237e9ef14c04d8e74ee463fd
cscendaya <carlieshaye24@gmail.com>
2025-03-11 15:44:31 +0800

    xor modifications except xor_cipher function

--- xor.py
+++ xor.py
@@ -5,48 +5,50 @@ def xor_cipher(txt_list, key_list):
     xor = []
     for i in range(len(txt_list)):
         xor.append(txt_list[i] ^ xkey[i])
-    return bytes(xor) #list of integers turned to bytes
+    return bytes(xor)                                                                 #list of integers turned to bytes
 
-def xor_encrypt(): #not final
-
+def xor_encrypt(): 
     try: 
-        xuser_input = input("XOR plaintext: ") #insert helper function? na may try except shizzles
-        xkey_input = input("XOR key: ")   #insert helper function?
-        xencrypted_text = xor_cipher(list(xuser_input.encode()), xkey_input.encode()) #list of bytes
-        print("Ciphertext(hex):", xencrypted_text.hex())                              #.hex() = normalize into a copy-able string
+        xuser_input = helper.xor_str_validation("XOR Plaintext: ")#.strip() #if valid, strip for end and start whitespace -- ilagay nlng to sa vigenere and caesar, whitespace is ok here masisira kasi message
     except ValueError as error: print(error)
 
-def xor_decrypt(): #kinda final
-    while True: 
-        try: 
-            xuser_input = input("XOR Ciphertext(hex): ")
-            if xuser_input == 'q': break            #if user wants to quit during input loop
-            xhex_input = bytes.fromhex(xuser_input) #bytes.fromhex() = from hex to byte 
-            break                     
-        except ValueError:
-            print("Invalid hexadecimal input. Please enter valid hex values.") 
+    try: 
+        xkey_input = helper.xor_str_validation("XOR key: ")   
+        xencrypted_text = xor_cipher(list(xuser_input.encode()), xkey_input.encode()) #return list of bytes (insert list() for consistency with xkeyinput repeating key??)
+        print("Ciphertext(hex):", xencrypted_text.hex())                              #.hex() = normalize into a copy-able string
+    except ValueError as error: print(error)
+    return
 
-    xkey_input = input("XOR Key: ").encode()  #insert helper function?
-    xdecrypted_text = xor_cipher(xhex_input, xkey_input) 
-    print("Decrypted text:", xdecrypted_text.decode())  
 
-def run():
+def xor_decrypt(): 
+    
+    try: 
+        xduser_input = helper.xor_str_validation("XOR Ciphertext: ")                                         
+        xhex_input = bytes.fromhex(xduser_input)                                       #bytes.fromhex() = from hex to byte                     
+    except ValueError:
+        print("Invalid hexadecimal input. Please enter valid hex values.")
+
+    try:
+        xkey_input = helper.xor_str_validation("XOR Key: ").encode()  
+        xdecrypted_text = xor_cipher(xhex_input, xkey_input) 
+        print("Decrypted text:", xdecrypted_text.decode()) 
+    except ValueError as error: print(error) 
+    return
+
+def run_xor():
 
     while True:
         print("""Version 1.0.0\n\nOrigin:
 
         The XOR ALGORITHM\n""")
-        
         choice = helper.get_menu_choice()
         
         if choice == 1:
             xor_encrypt()  
-            helper.get_menu_choice() 
         elif choice == 2:
             xor_decrypt() 
-            helper.get_menu_choice()      
         elif choice == 3: 
             print("Returning to the Main Menu...")
             break  
-                                
+                                    
 
